1110cd96a5a9a2cd9c702f0c570600d9f6d89eb4
dtan4 <dtan4@mac>
2013-08-21 22:40:48 +0900

    Generate articles

---
+++ lib/blog.rb
@@ -0,0 +1,20 @@
+require 'sinatra/base'
+require 'ostruct'
+require 'time'
+
+class Blog < Sinatra::Base
+  set :root, File.expand_path('../../', __FILE__)
+
+  Dir.glob "#{root}/articles/*.md" do |file|
+    meta, content = File.read(file).split("\n\n", 2)
+
+    article = OpenStruct.new YAML.load(meta)
+    article.date = Time.parse article.date.to_s
+    article.content = content
+    article.slug = File.basename(file, '.md')
+
+    get "/#{article.slug}" do
+      erb :post, locals: { article: article }
+    end
+  end
+end
